e3e61a5997fe1c9088123576690124e789f3cf3e
choco <longhorn1102@gmail.com>
2023-05-31 17:53:37 +0900

    the surf

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,49 @@ struct State {
 
 impl State {
     async fn new(window: Window) -> Self {
-        todo!()
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let adapter = instance.request_adapter(
+            &wgpu::RenderAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+                label: None,
+            },
+            None,
+        ).await.unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps.formats.iter()
+            .copied()
+            .find(|f| f.describe().srgb)
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],            
+        };
+        surface.configure(&device, &config);
+        
     }
 
     pub fn window(&self) -> &Window {
@@ -28,7 +70,7 @@ impl State {
         todo!()
     }
     
-    fn input(mut self) {
+    fn update(mut self) {
         todo!()
     }
 
