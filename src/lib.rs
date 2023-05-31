552c722b23a85f096e6dd394bd13197795ce5890
choco <longhorn1102@gmail.com>
2023-05-31 16:05:14 +0900

    the surf

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use winit::{
+    window::{Window, WindowBuilder},
+};
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl State {
+    async fn new(window: Window) -> Self {
+        todo!()
+    }
+    
+}
