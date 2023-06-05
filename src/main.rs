b7db1a6cadc1926de77d3c5f6a62db5d0ca2d4bc
rascal-chan <longhorn1102@gmail.com>
2023-06-05 23:29:09 +0900

    emu

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use getopts::Options;
+use std::env;
 
-use nesnes::*;
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
-fn main() {    
-    pollster::block_on(run());
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let output = matches.opt_str("o");
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
+    
 }
