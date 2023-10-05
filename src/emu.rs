e7445266b142a7a323dbdb82a8739249e13acb79
choco <longhorn1102@gmail.com>
2023-10-05 17:05:44 +0900

    window winit init

--- src/emu.rs
+++ src/emu.rs
@@ -1,5 +1,13 @@
 use crate::{cpu::Cpu, mem::Mem};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{WindowBuilder, Window}, dpi::LogicalSize,
+};  
 
+const WIDTH: u32  = 24;
+const HEIGHT: u32 = 24;
+const SCALE: f64  = 30.0;
 pub struct Emu {
     pub stat: Stat,
     pub cpu: Cpu,
@@ -34,5 +42,30 @@ impl Emu {
     pub fn load(&mut self, bin: Vec<u8>) {
         self.mem.copy_from_slice(0x8000, &bin);
     }
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) {
+        let event_loop = EventLoop::new();
+        let window = {
+            let size = LogicalSize::new( WIDTH as f64, HEIGHT as f64);
+            let scaled_size = LogicalSize::new( WIDTH as f64 * SCALE, HEIGHT as f64 * SCALE);
+            WindowBuilder::new()
+                .with_title("EASY2A03")
+                .with_inner_size(size)
+                .with_min_inner_size(scaled_size)
+                .build(&event_loop)
+                .unwrap()
+        };
+        event_loop.run(move |event, _, control_flow| {
+            control_flow.set_poll();
+            control_flow.set_wait();
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    control_flow.set_exit();
+                },
+                _ => (),
+            }
+        });
+    }
 }
